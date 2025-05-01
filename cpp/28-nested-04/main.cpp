ea0a8b16d63c9a10910a3a25250732a3db09ef1b
noor fatima <kaspi0965@gmail.com>
2025-05-01 11:11:02 -0700

    cpp

---
+++ cpp/28-nested-04/main.cpp
@@ -0,0 +1,25 @@
+/*
+04:
+Star Pattern – Right Triangle
+*
+* *
+* * *
+* * * *
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
