d8c361f86635cc7dc4c1f964ae5a06e354beae19
noor fatima <kaspi0965@gmail.com>
2025-05-01 09:59:18 -0700

    cpp

---
+++ cpp/24-nested-loop/index.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 2; j++)
+        {
+            cout<<"outer"<<i<<"inner"<<j<<endl;
+        }
+        
+    }
+    
+    
+    return 1;
+}
---
+++ cpp/25-nested-01/index.cpp
@@ -0,0 +1,22 @@
+/*
+01: Print Pattern (Rectangle of Stars)
+*****
+*****
+*****
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
