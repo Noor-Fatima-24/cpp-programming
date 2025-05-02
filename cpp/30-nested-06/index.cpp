802ea76978a717c8ee0504eebcf2348f10d2bb55
noor fatima <kaspi0965@gmail.com>
2025-05-02 11:17:42 -0700

    cpp

--- cpp/30-nested-06/index.cpp
+++ cpp/30-nested-06/index.cpp
@@ -1,8 +1,31 @@
-#include<iostream>
+/*
+06:
+    Print a Pyramid Pattern
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+*/
+
+    #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+    for (int i = 0; i < 5; i++)
+    {
+        for (int s = 0; s < 5-i; s++)
+        {
+            cout<<" ";
+        }
+        for (int j = 0; j < 2*i - 1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
     
     return 0;
 }
+
+ 
