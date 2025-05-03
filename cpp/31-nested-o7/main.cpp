f3a0a9d02e20f401fdb91a5c5341b234a58abfc9
noor fatima <kaspi0965@gmail.com>
2025-05-02 23:26:49 -0700

    cpp

--- cpp/31-nested-o7/main.cpp
+++ cpp/31-nested-o7/main.cpp
@@ -13,13 +13,16 @@
 using namespace std;
 
 int main() {
-    int num = 1;
-    for(int i = 1; i <= 5; i++) {
-        for(int j = 1; j <= i; j++) {
-            cout << num << " ";
-            num++;
-        }
-        cout << endl;
+    int count=1;
+   for (int  i = 1; i <= 5; i++)
+   {
+    for (int j = 1; j <= i; j++)
+    {
+        cout<<count<<" ";
+        count++;
     }
+    cout<<endl;
+   }
+   
     return 0;
 }
