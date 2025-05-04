f599920152b0eb5067d233c846b98403c4a2c97d
noor fatima <kaspi0965@gmail.com>
2025-05-04 14:00:13 -0700

    cpp

---
+++ cpp/32-nested-09/mian.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int c=6;
+    for (int i = 0; i < 2; i++)
+   {
+    for ( int j = 0; j < 3; j++)
+   {
+    cout<<c;
+    c--;
+   }
+   cout<<endl;
+   }
+    return 0;
+}
