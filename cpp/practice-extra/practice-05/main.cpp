5e72e9c918b0f3f60f5984229b08d684137cfab7
noor fatima <kaspi0965@gmail.com>
2025-05-08 10:42:46 -0700

    cpp

--- cpp/practice-extra/practice-05/main.cpp
+++ cpp/practice-extra/practice-05/main.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main()
 {
+    for (int i = 0; i < 5; i++)
+    {
+        for (int s = 0; s < 5-i; s++)
+        {
+            cout<<" ";
+        }
+        
+        for (int j = 0; j < i; j++)
+        {
+            cout<<" @" ;
+        }
+        cout<<endl;
+    }
     
     return 0;
 }
