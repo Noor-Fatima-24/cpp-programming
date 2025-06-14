5495c32ff74f94f8d01374e2b65c9bde8e9552e2
noor fatima <kaspi0965@gmail.com>
2025-06-14 16:05:57 +0500

    cpp

---
+++ cpp/practice-extra/Q1/main.cpp
@@ -0,0 +1,13 @@
+//Print numbers from 1 to 100 using a loop.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
