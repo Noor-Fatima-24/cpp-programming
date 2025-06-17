e2304375d5fbaffcd5773e6ad87cde1269f7ed0f
noor fatima <kaspi0965@gmail.com>
2025-06-18 00:32:28 +0500

    cpp

---
+++ cpp/practice-extra/13/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5],sum=0;
+    cout<<"Enter 5 numbers : ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>arr[i];
+        sum += arr[i];
+    }
+    float avg = sum/5.0;
+    cout<<"sum = "<<sum<<" avg = "<<avg;
+
+    
+    return 0;
+}
