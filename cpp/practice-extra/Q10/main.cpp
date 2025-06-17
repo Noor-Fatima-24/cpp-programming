d4c1c7c5b64c5687cc6cbf9f1358cd5036990bcf
noor fatima <kaspi0965@gmail.com>
2025-06-18 00:14:09 +0500

    cpp

--- cpp/practice-extra/Q10/main.cpp
+++ cpp/practice-extra/Q10/main.cpp
@@ -1,62 +1,37 @@
-// #include<iostream>
-// using namespace std;
-
-// int sum(int arr[5],int n,int key,bool check=true)
-// {
-//      n=0;
-//     for (int i = 0; i <= 4; i++)
-//     {
-//         if (arr[i]==key)
-//         {
-//             return i;
-//             check=false;
-//             break;
-//         }
-        
-//     }
-//     if (check==true)
-//     {
-//         cout<< "not in arrya";
-//     }
-    
-//         return -1;
-          
-    
-// }
-
-// int main()
-// {
-//     int arr[5],n,key;
-//     cout<<"E";
-
-//     for (int i = 0; i <=4; i++)
-//     {
-//         cin>>arr[i];
-//     }
-//     cout<<"key";
-//     cin>>key;
-//     cout<<sum(arr,n,key);
-//     return 0;
-// }
-//pointers
-// #include<iostream>
-// using namespace std;
-
-// int main()
-// {
-//      int a = 10;           // normal variable
-//     int* p = &a;          // pointer p stores the address of a
-//     return 0;
-// }
 #include<iostream>
 using namespace std;
 
-int main() {
-    int x = 20;
-    int* ptr = &x;
-  cout<<"p"<<ptr<<endl;
-    *ptr = 50;  // change value of x using pointer
-cout<<"n"<<ptr;
-    cout << "New value of x: " << x;  // Output: 50
+// Function to find the index of key in array
+int sum(int arr[5], int key)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] == key)
+        {
+            return i;  // Return index if key found
+        }
+    }
+    // If not found
+    cout << "Key not in array" << endl;
+    return -1;
+}
+
+int main()
+{
+    int arr[5], key;
+    cout << "Enter 5 array elements: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter key to search: ";
+    cin >> key;
+
+    int result = sum(arr, key);
+
+    if (result != -1)
+        cout << "Key found at index: " << result;
+    
     return 0;
 }
---
+++ cpp/practice-extra/Q10/tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+    int arr[5],n,key;
+    cout<<"E";
+
+    for (int i = 0; i <=4; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"key";
+    cin>>key;
+    cout<<sum(arr,n,key);
+    return 0;
+}
+
