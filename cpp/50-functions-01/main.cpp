f02504108b584ed0f9c97d7157426189d5ddb64d
noor fatima <kaspi0965@gmail.com>
2025-06-28 01:19:26 +0500

    cpp

---
+++ cpp/50-functions-01/main.cpp
@@ -0,0 +1,13 @@
+// In case u want to place your functions code after the main so it is important to declare it first
+
+#include<iostream>
+using namespace std;
+
+// Function Declaration (Prototype)
+return_type function_name(parameter_list);
+
+int main()
+{
+    
+    return 0;
+}
