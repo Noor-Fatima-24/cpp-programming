7027bec400f5b94071a6cf9b9e0433eb36ed519a
noor fatima <kaspi0965@gmail.com>
2025-11-14 22:51:28 +0500

    CPP-PROGRAMMING

--- revision_cpp/01/main.cpp
+++ revision_cpp/01/main.cpp
@@ -8,3 +8,7 @@ int main()
   
   return 0;
 }
+// cout
+// Function: It displays information from the program to the user (on the screen).
+// How to remember: "Console OUT"
+// Symbol: << (like arrows pointing away from the program, towards the screen)
