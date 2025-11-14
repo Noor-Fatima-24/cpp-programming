b3f2162f3c2cb28e40ce9067e7af05267dbc8699
noor fatima <kaspi0965@gmail.com>
2025-11-14 22:50:27 +0500

    CPP-PROGRAMMING

---
+++ revision_cpp/02/main.cpp
@@ -0,0 +1,21 @@
+// cin ("Console IN")
+// Function: It takes information from the user into the program.
+// Symbol: >> (like arrows pointing into the program, away from the user)
+
+
+// cout
+// sizeof()
+// data types
+// arthimatic operators
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a =2, b=4 ;
+    cout<< a*b ;
+    return 0;
+}
+
