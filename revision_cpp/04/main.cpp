11e609ce8a90b008bc84b6b30a9ebd57e5447529
noor fatima <kaspi0965@gmail.com>
2025-11-23 16:30:40 +0500

    CPP-PROGRAMMING

---
+++ revision_cpp/04/main.cpp
@@ -0,0 +1,32 @@
+// Statement:
+
+/*
+Write a C++ program to demonstrate the working of unary operators.
+Show the difference between pre-increment, post-increment, pre-decrement
+ and post-decrement on an integer variable.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x = 5;
+
+    cout << "Original x = " <<x<< endl;
+//x=5
+    cout << "Post Increment (x++) = " << x++ << endl;   // prints old value
+    cout << "After Post Increment x = " << x << endl;
+//x=6
+//x=7
+    cout << "Pre Increment (++x) = " << ++x << endl;    // prints new value
+//x=6
+    cout << "Post Decrement (x--) = " << x-- << endl;
+    cout << "After Post Decrement x = " << x << endl;
+//x=5
+    cout << "Pre Decrement (--x) = " << --x << endl;
+
+    return 0;
+}
+
+
---
+++ revision_cpp/05/main.cpp
@@ -0,0 +1,27 @@
+/*
+Statement:
+Write a C++ program to calculate the area and diameter of a circle using a constant value PI.
+Use arithmetic operators (+, *, /) to perform calculations.    */
+
+// area = PI * radius * radius
+// diameter = 2 * radius 
+// radius = diameter / 2  ( if not given ) 
+
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+    const float PI = 3.14;   // Constant value by const qualifer declaration mehthod
+
+    float radius = 6.0;
+
+    // Arithmetic operators: +, -, *, /
+    float area = PI * radius * radius;
+    float diameter = 2 * radius;
+
+    cout << "Area: " << area << endl;
+    cout << "Diameter: " << diameter << endl;
+
+    return 0;
+}
