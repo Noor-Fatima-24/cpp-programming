35ea5502dd4654a57e7acce5ec0f59a3bbe5b9e1
noor fatima <kaspi0965@gmail.com>
2026-01-10 11:24:01 +0500

    CPP-PROGRAMMING

---
+++ cpp/60-IF/main.cpp
@@ -0,0 +1,20 @@
+//Example 2: Check if a number is even
+// exmaple : check if a naumber is even using " if " in cpp.
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n % 2 == 0) 
+    {
+        cout << "The number is even";
+    }
+
+    return 0;
+}
+
+
