0bc2a26360debfb199b68db51a25f2b6b91457c7
noor fatima <kaspi0965@gmail.com>
2026-01-29 20:21:54 +0500

    CPP-PROGRAMMING

---
+++ cpp/72-nested_loop_03/main.cpp
@@ -0,0 +1,27 @@
+/* Inverted Pyramid Pattern
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 5; i > 0; i--)
+    {
+        for (int p = 0; p < i ; p++)
+        {
+            cout << " *";
+        }
+        
+        cout<<endl;
+    }
+    
+    return 0;
+}
